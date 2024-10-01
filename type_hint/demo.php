0634514fcbc2bebaf9ca1012aab65a57e3d5d3b0
vansieu2502 <nguyenvansieu2502@gmail.com>
2024-10-01 09:07:06 +0700

    update

--- type_hint/demo.php
+++ type_hint/demo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 include 'I.php';
@@ -6,7 +6,8 @@ include 'B.php';
 include 'C.php';
 include 'A.php';
 
- class Demo {
+class Demo
+{
     //A
     public function typeReturnA(): ?A
     {
@@ -18,137 +19,137 @@ include 'A.php';
         echo __FUNCTION__ . "<br>";
         return new B;
     }
-    public function typeReturnC(): ?A
-    {
-        echo __FUNCTION__ . "<br>";
-        return new C;
-    }
-    public function typeReturnI(): ?A
-    {
-        echo __FUNCTION__ . "<br>";
-        return new I;
-    }
-    public function typeReturnNull(): ?A
-    {
-        echo __FUNCTION__ . "<br>";
-        return new Null;
-    }
+    // public function typeReturnC(): ?A
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new C;
+    // }
+    // public function typeReturnI(): ?A
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new I;
+    // }
+    // public function typeReturnNull(): ?A
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new Null;
+    // }
 
-//B
+    // //B
 
-    public function typeReturnA(): ?B
-    {
-        echo __FUNCTION__ . "<br>";
-        return new A;
-    }
+    // public function typeReturnA(): ?B
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new A;
+    // }
 
-   public function typeReturnB(): ?B
-    {
-        echo __FUNCTION__ . "<br>";
-        return new B;
-    }
-    public function typeReturnC(): ?B
-    {
-        echo __FUNCTION__ . "<br>";
-        return new C;
-    }
-    public function typeReturnI():B
-    {
-        echo __FUNCTION__ . "<br>";
-        return new I;
-    }
-    public function typeReturnNull():B
-    {
-        echo __FUNCTION__ . "<br>";
-        return new Null;
-    }
-    
-    public function typeReturnA(): ?C
-    {
-        echo __FUNCTION__ . "<br>";
-        return new A;
-    }
-    public function typeReturnB(): ?C
-    {
-        echo __FUNCTION__ . "<br>";
-        return new B;
-    }
-    public function typeReturnC(): ?C
-    {
-        echo __FUNCTION__ . "<br>";
-        return new C;
-    }
-    public function typeReturnI(): ?C
-    {
-        echo __FUNCTION__ . "<br>";
-        return new I;
-    }  
-    public function typeReturnNull(): ?C
-    {
-        echo __FUNCTION__ . "<br>";
-        return new Null;
-    }
+    // public function typeReturnB(): ?B
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new B;
+    // }
+    // public function typeReturnC(): ?B
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new C;
+    // }
+    // public function typeReturnI(): ?B
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new I;
+    // }
+    // public function typeReturnNull(): ?B
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new Null;
+    // }
 
-    //I
+    // public function typeReturnA(): ?C
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new A;
+    // }
+    // public function typeReturnB(): ?C
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new B;
+    // }
+    // public function typeReturnC(): ?C
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new C;
+    // }
+    // public function typeReturnI(): ?C
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new I;
+    // }
+    // public function typeReturnNull(): ?C
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new Null;
+    // }
 
-     public function typeReturnA(): ?I
-    {
-        echo __FUNCTION__ . "<br>";
-        return new A;
-    }
-    public function typeReturnB(): ?I
-    {
-        echo __FUNCTION__ . "<br>";
-        return new B;
-    }
-    public function typeReturnC(): ?I
-    {
-        echo __FUNCTION__ . "<br>";
-        return new C;
-    }
-    public function typeReturnI(): ?I
-    {
-        echo __FUNCTION__ . "<br>";
-        return new I;
-    }
-    public function typeReturnNull(): ?I
-    {
-        echo __FUNCTION__ . "<br>";
-        return new Null;
-    }
-   //null
-   public function typeReturnNull(): ?A
-   {
-       echo __FUNCTION__ . "<br>";
-       return new A;
-   }
-   public function typeReturnNull(): ?B
-   {
-       echo __FUNCTION__ . "<br>";
-       return new B;
-   }
-   public function typeReturnNull(): ?C
-   {
-       echo __FUNCTION__ . "<br>";
-       return new C;
-   }
-  public function typeReturnI(): ?I
-    {
-        echo __FUNCTION__ . "<br>";
-        return new I;
-    }
-    public function typeReturnNull(): ?Null
-    {
-        echo __FUNCTION__ . "<br>";
-        return new Null;
-    }
- }
- $demo =new Demo();
+    // //I
 
-// $demo->typeReturnA();
+    // public function typeReturnA(): ?I
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new A;
+    // }
+    // public function typeReturnB(): ?I
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new B;
+    // }
+    // public function typeReturnC(): ?I
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new C;
+    // }
+    // public function typeReturnI(): ?I
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new I;
+    // }
+    // public function typeReturnNull(): ?I
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new Null;
+    // }
+    // //null
+    // public function typeReturnNull(): ?A
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new A;
+    // }
+    // public function typeReturnNull(): ?B
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new B;
+    // }
+    // public function typeReturnNull(): ?C
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new C;
+    // }
+    // public function typeReturnI(): ?I
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new I;
+    // }
+    // public function typeReturnNull(): ?null
+    // {
+    //     echo __FUNCTION__ . "<br>";
+    //     return new Null;
+    // }
+}
+$demo = new Demo();
+
+$demo->typeReturnA();
 // $demo->typeReturnB();
 // $demo->typeReturnC();
 // $demo->typeReturnI();
-$demo->typeReturnNUll();
- 
+// $demo->typeReturnNUll();
+
 ?>
